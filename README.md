54880ebc41cbec1a3ef8ea980dcfebc5cb1fa9b3
LeCadavrExquis <filipzarnowiec@gmail.com>
2023-12-09 18:23:55 +0100

    [WIP] Konfiguracja SimGrid

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-#Asynchroniczna implementacja iteracyjnego algorytmu Jacobiniego
+# Asynchroniczna implementacja iteracyjnego algorytmu Jacobiniego
+
+## Instalacja MPI
+
+``sudo apt-get -y install openmpi-common``
+
+### Symulacja systemu z pamięcią rozproszoną (SimGrid)
+
+``sudo apt install simgrid pajeng make gcc g++ gfortran python3 vite``
--- main.c
+++ main.c
@@ -2,11 +2,55 @@
 #include <stdlib.h>
 #include <time.h>
 #include <string.h>
+#include <sys/time.h>
 #include "utils.h"
+#include "mpi.h"
 #include "alg.h"
 
-int main() {
+#define N (1024 * 1024 * 1)
+
+int main(int argc, char* argv[]) {
+    int process_Rank, size_Of_Cluster;
+
+    MPI_Init(&argc, &argv);
+    MPI_Comm_size(MPI_COMM_WORLD, &size_Of_Cluster);
+    MPI_Comm_rank(MPI_COMM_WORLD, &process_Rank);
+
+    printf("Hello World from process %d of %d\n", process_Rank, size_Of_Cluster);
     int MATRIX_SIZE = 1000;
+
+    int size, rank;
+    struct timeval start, end;
+    char hostname[256];
+    int hostname_len;
+
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+    MPI_Get_processor_name(hostname, &hostname_len);
+
+    // Allocate a 1 MiB buffer
+    char* buffer = malloc(MATRIX_SIZE * sizeof(char));
+
+    // Communicate along the ring
+    if (rank == 0) {
+        gettimeofday(&start, NULL);
+        printf("Rank %d (running on '%s'): sending the message rank %d\n", rank, hostname, 1);
+        MPI_Send(buffer, N, MPI_BYTE, 1, 1, MPI_COMM_WORLD);
+        MPI_Recv(buffer, N, MPI_BYTE, size - 1, 1, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+        printf("Rank %d (running on '%s'): received the message from rank %d\n", rank, hostname, size - 1);
+        gettimeofday(&end, NULL);
+        printf("%f\n", (end.tv_sec * 1000000.0 + end.tv_usec - start.tv_sec * 1000000.0 - start.tv_usec) / 1000000.0);
+
+    } else {
+        MPI_Recv(buffer, N, MPI_BYTE, rank - 1, 1, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+        printf("Rank %d (running on '%s'): receive the message and sending it to rank %d\n", rank, hostname,
+               (rank + 1) % size);
+        MPI_Send(buffer, N, MPI_BYTE, (rank + 1) % size, 1, MPI_COMM_WORLD);
+    }
+
+    MPI_Finalize();
+    return 0;
+
     printf("Generowanie macierzy %d x %d\n", MATRIX_SIZE, MATRIX_SIZE);
     double **matrix = malloc(MATRIX_SIZE * sizeof(double *));
     for (int i = 0; i < MATRIX_SIZE; i++) {
